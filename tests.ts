e6914e59bb4fdcc0b72797999d85a4e1c8f62b55
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-08-28 19:19:06 +0200

    add

---
+++ tests.ts
@@ -0,0 +1,12 @@
+import solution = require('./solution');
+// import the type of assertion library you wish to use (Chai recommended)
+import {assert} from "chai";
+
+describe("Sample Test Cases", function(){
+  it("Should return a string", function() {
+    assert.equal(solution.solution('world'), 'dlrow');
+    assert.equal(solution.solution('hello'), 'olleh');
+    assert.equal(solution.solution(''), '');
+    assert.equal(solution.solution('h'), 'h');
+  });
+});
